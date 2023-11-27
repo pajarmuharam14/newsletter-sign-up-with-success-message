0bb59271d6aff3b26f72ee69051e6d5cda9838fb
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-11-27 17:09:56 +0700

    add javascript code to project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const formEmail = document.querySelector("#form-email");
+const emailInput = document.querySelector("#email");
+const mainForm = document.querySelector(".main-form");
+const succsessMessages = document.querySelector(".success-message");
+const invalidMessage = document.querySelector(".invalid-message");
+
+function validateEmail(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  return regex.test(email);
+}
+
+formEmail.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const invalidMessageColor = "hsl(4,100%,67%)";
+
+  if (validateEmail(emailInput.value)) {
+    mainForm.style.display = "none";
+    succsessMessages.style.display = "flex";
+  } else {
+    emailInput.style.borderColor = invalidMessageColor;
+    emailInput.style.color = invalidMessageColor;
+    invalidMessage.style.display = "inline";
+  }
+});
