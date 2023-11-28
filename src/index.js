702d22931e37632536b12225eeb28113a57e7316
pajarmuharam14 <pajarmuharam321@gmail.com>
2023-11-28 08:16:15 +0700

    fix javascript code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const emailInput = document.querySelector("#email");
 const mainForm = document.querySelector(".main-form");
 const succsessMessages = document.querySelector(".success-message");
 const invalidMessage = document.querySelector(".invalid-message");
+const DismissMessageBtn = document.querySelector("#btn-dismiss");
 
 function validateEmail(email) {
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
@@ -16,11 +17,17 @@ formEmail.addEventListener("submit", (e) => {
   const invalidMessageColor = "hsl(4,100%,67%)";
 
   if (validateEmail(emailInput.value)) {
-    mainForm.style.display = "none";
-    succsessMessages.style.display = "flex";
+    mainForm.classList.replace("flex", "hidden");
+    succsessMessages.classList.replace("hidden", "flex");
+    e.target.reset();
   } else {
     emailInput.style.borderColor = invalidMessageColor;
     emailInput.style.color = invalidMessageColor;
     invalidMessage.style.display = "inline";
   }
+});
+
+DismissMessageBtn.addEventListener("click", () => {
+  succsessMessages.classList.replace("flex", "hidden");
+  mainForm.classList.replace("hidden", "flex");
 });
